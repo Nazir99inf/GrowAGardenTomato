009e79af7b1fc81031436f0d5d48cb58b78180d4
Nazir⚡ <nazir99iq@gmail.com>
2025-06-25 20:19:26 +0800

    Update tomato.lua

--- tomato.lua
+++ tomato.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet(""))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nazir99inf/GrowAGardenTomato/refs/heads/main/game.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
